a09fd9c1663edfc177fd732397cbf547d5d2575b
Stefan Henneken <stefan.henneken@live.com>
2017-06-07 22:11:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Sample05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+        public void Run()
+        {
+            Console.WriteLine("Start Run");
+
+            double result01 = 0;
+            for (int i = 1; i < 10000; i++)
+                result01 += DoSomeWork(i);
+            Console.WriteLine("Result: {0}", result01);
+
+            double result02 = 0;
+            result02 = ParallelEnumerable.Range(0, 10000).Sum(i => DoSomeWork(i));
+            Console.WriteLine("Result: {0}", result02);
+
+            double result03 = 0;
+            result03 = (from x in ParallelEnumerable.Range(0, 10000).AsParallel()
+                        select x).Sum(i => DoSomeWork(i));
+            Console.WriteLine("Result: {0}", result03);
+
+            Console.WriteLine("End Run");
+            Console.ReadLine();
+        }
+        private double DoSomeWork(int index)
+        {
+            return Math.Sin(index) + Math.Sqrt(index) * Math.Pow(index, 0.14);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sample05")]
+[assembly: AssemblyDescription("www.StefanHenneken.wordpress.com")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Stefan Henneken")]
+[assembly: AssemblyProduct("TPL Teil 6 - Aggregationen")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4d8ecf85-92dd-4938-a8e7-8c1bf034126c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
